66102ae8bdbbcb3e0e110d9918d59d9f7a1b28ea
christopher <iamchris042014@gmail.com>
2024-02-01 17:40:54 -0500

    fixed online tabs toggle js

--- js/main.js
+++ js/main.js
@@ -13,24 +13,24 @@ function printdiv(elem) {
  
 // ONLINE PAGE TABS --------------------------------------------------------------------------------------------------------------------------------------
 
-function openSubject(evt, schoolName) {
+function openDiscipline(evt, collegeName) {
   // Declare all variables
-  var i, tabcontent1, tablinks1;
+  var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent1" and hide them
-  tabcontent1 = document.getElementsByClassName("tabcontent1");
-  for (i = 0; i < tabcontent1.length; i++) {
-    tabcontent1[i].style.display = "none";
+  tabcontent = document.getElementsByClassName("tabcontent1");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks1" and remove the class "active1"
-  tablinks1 = document.getElementsByClassName("tablinks1");
-  for (i = 0; i < tablinks1.length; i++) {
-    tablinks1[i].className = tablinks1[i].className.replace(" active1", "");
+  tablinks = document.getElementsByClassName("tablinks1");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active1", "");
   }
 
   // Show the current tab, and add an "active1" class to the button that opened the tab
-  document.getElementById(schoolName).style.display = "block";
+  document.getElementById(collegeName).style.display = "block";
   evt.currentTarget.className += " active1";
 }
 
@@ -38,49 +38,49 @@ function openSubject(evt, schoolName) {
 
 //MODAL
 // // Get the modal
-// var modal = document.getElementById("dual-modal");
+var modal = document.getElementById("dual-modal");
 
 // // Get the button that opens the modal
-// var btn = document.getElementById("dual-button");
+var btn = document.getElementById("dual-button");
 
 // // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close1")[0];
+var span = document.getElementsByClassName("close1")[0];
 
 // // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-  // modal.style.display = "block";
-// }
+btn.onclick = function() {
+  modal.style.display = "block";
+};
 
 // // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-  // modal.style.display = "none";
-// }
+span.onclick = function() {
+  modal.style.display = "none";
+};
 
 // // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-  // if (event.target == modal) {
-    // modal.style.display = "none";
-  // }
-// }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 //TOGGLE MENU
 function openCriteria(evt, collegeName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
+  // Get all elements with class="tabcontent2" and hide them
   tabcontent = document.getElementsByClassName("tabcontent2");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active1"
+  // Get all elements with class="tablinks2" and remove the class "active2"
   tablinks = document.getElementsByClassName("tablinks2");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active2", "");
   }
 
-  // Show the current tab, and add an "active1" class to the button that opened the tab
+  // Show the current tab, and add an "active2" class to the button that opened the tab
   document.getElementById(collegeName).style.display = "block";
   evt.currentTarget.className += " active2";
 }
@@ -91,19 +91,19 @@ function openSubject(evt, collegeName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
+  // Get all elements with class="tabcontent3" and hide them
   tabcontent = document.getElementsByClassName("tabcontent3");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active1"
+  // Get all elements with class="tablinks3" and remove the class "active1"
   tablinks = document.getElementsByClassName("tablinks3");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active3", "");
   }
 
-  // Show the current tab, and add an "active1" class to the button that opened the tab
+  // Show the current tab, and add an "active3" class to the button that opened the tab
   document.getElementById(collegeName).style.display = "block";
   evt.currentTarget.className += " active3";
 }
